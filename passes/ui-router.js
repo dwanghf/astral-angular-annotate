f30d222e64df9a2e9d8668d58899171ee730a95d
David Wang <dwanghf@highfive.com>
2014-03-31 15:42:49 -0700

    forgot to include it in the main annotate.js

--- passes/ui-router.js
+++ passes/ui-router.js
@@ -3,169 +3,169 @@
 var annotateInjectable = require('../lib/annotate-injectable');
 var deepApply = require('../lib/deep-apply');
 
-module.exports = {
-	name: 'angular:annotator:ui-router',
-	prereqs: [
-		'angular:annotator:mark'
-	],
-	run: function (ast, info) {
-		deepApply(ast, [{
-			"type": "CallExpression",
-			"callee": {
-				"type": "MemberExpression",
-				"object": {
-					"ngModule": true
-				},
-				"property": {
-					"type": "Identifier",
-					"name": "config"
-				}
-			}
-		}], function (routeChunk) {
-			deepApply(routeChunk, [{
-				"type": "CallExpression",
-				"callee": {
-					"type": "MemberExpression",
-					"property": {
-						"type": "Identifier",
-						"name": "state"
-					}
-				},
-				"arguments": [ {},
-				{
-					"type": "ObjectExpression"
-				}
-				]
-			}], function (stateChunk) {
-				// templateProvider annotations
-				deepApply(stateChunk, [{
-					"type": "Property",
-					"key": {
-						"type": "Identifier",
-						"name": "templateProvider"
-					},
-					"value": {
-						"type": "FunctionExpression"
-					}
-				}], function (templateProviderIdentifierChunk) {
-					templateProviderIdentifierChunk.value = annotateInjectable(templateProviderIdentifierChunk.value);
-				});
+var uiRouterAnnotatorPass = module.exports = {};
+uiRouterAnnotatorPass.name = 'angular:annotator:ui-router';
+uiRouterAnnotatorPass.prereqs = [
+  'angular:annotator:mark'
+];
 
-				// controller annotations
-				deepApply(stateChunk, [{
-					"type": "Property",
-					"key": {
-						"type": "Identifier",
-						"name": "controller"
-					},
-					"value": {
-						"type": "FunctionExpression"
-					}
-				}], function (controllerIdentifierChunk) {
-					controllerIdentifierChunk.value = annotateInjectable(controllerIdentifierChunk.value);
-				});
+uiRouterAnnotatorPass.run = function (ast, info) {
+  deepApply(ast, [{
+    "type": "CallExpression",
+    "callee": {
+      "type": "MemberExpression",
+      "object": {
+        "ngModule": true
+      },
+      "property": {
+        "type": "Identifier",
+        "name": "config"
+      }
+    }
+  }], function (routeChunk) {
+    deepApply(routeChunk, [{
+      "type": "CallExpression",
+      "callee": {
+        "type": "MemberExpression",
+        "property": {
+          "type": "Identifier",
+          "name": "state"
+        }
+      },
+      "arguments": [ {},
+      {
+        "type": "ObjectExpression"
+      }
+      ]
+    }], function (stateChunk) {
+      // templateProvider annotations
+      deepApply(stateChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "templateProvider"
+        },
+        "value": {
+          "type": "FunctionExpression"
+        }
+      }], function (templateProviderIdentifierChunk) {
+        templateProviderIdentifierChunk.value = annotateInjectable(templateProviderIdentifierChunk.value);
+      });
 
-				// controllerProvider annotations
-				deepApply(stateChunk, [{
-					"type": "Property",
-					"key": {
-						"type": "Identifier",
-						"name": "controllerProvider"
-					},
-					"value": {
-						"type": "FunctionExpression"
-					}
-				}], function (controllerProviderIdentifierChunk) {
-					controllerProviderIdentifierChunk.value = annotateInjectable(controllerProviderIdentifierChunk.value);
-				});
+      // controller annotations
+      deepApply(stateChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "controller"
+        },
+        "value": {
+          "type": "FunctionExpression"
+        }
+      }], function (controllerIdentifierChunk) {
+        controllerIdentifierChunk.value = annotateInjectable(controllerIdentifierChunk.value);
+      });
 
-				// onEnter annotations
-				deepApply(stateChunk, [{
-					"type": "Property",
-					"key": {
-						"type": "Identifier",
-						"name": "onEnter"
-					},
-					"value": {
-						"type": "FunctionExpression"
-					}
-				}], function (onEnterIdentifierChunk) {
-					onEnterIdentifierChunk.value = annotateInjectable(onEnterIdentifierChunk.value);
-				});
+      // controllerProvider annotations
+      deepApply(stateChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "controllerProvider"
+        },
+        "value": {
+          "type": "FunctionExpression"
+        }
+      }], function (controllerProviderIdentifierChunk) {
+        controllerProviderIdentifierChunk.value = annotateInjectable(controllerProviderIdentifierChunk.value);
+      });
 
-				// onExit annotations
-				deepApply(stateChunk, [{
-					"type": "Property",
-					"key": {
-						"type": "Identifier",
-						"name": "onExit"
-					},
-					"value": {
-						"type": "FunctionExpression"
-					}
-				}], function (onExitIdentifierChunk) {
-					onExitIdentifierChunk.value = annotateInjectable(onExitIdentifierChunk.value);
-				});
+      // onEnter annotations
+      deepApply(stateChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "onEnter"
+        },
+        "value": {
+          "type": "FunctionExpression"
+        }
+      }], function (onEnterIdentifierChunk) {
+        onEnterIdentifierChunk.value = annotateInjectable(onEnterIdentifierChunk.value);
+      });
 
-				// resolve annoations
-				deepApply(stateChunk, [{
-					"type": "Property",
-					"key": {
-						"type": "Identifier",
-						"name": "resolve"
-					},
-					"value": {
-						"type": "ObjectExpression"
-					}
-				}], function (resolveChunk) {
-					deepApply(resolveChunk, [{
-						"type": "Property",
-						"key": {
-							"type": "Identifier"
-						},
-						"value": {
-							"type": "FunctionExpression"
-						}
-					}], function (resolveIdentifierChunk) {
-						resolveIdentifierChunk.value = annotateInjectable(resolveIdentifierChunk.value);
-					});
-				});
+      // onExit annotations
+      deepApply(stateChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "onExit"
+        },
+        "value": {
+          "type": "FunctionExpression"
+        }
+      }], function (onExitIdentifierChunk) {
+        onExitIdentifierChunk.value = annotateInjectable(onExitIdentifierChunk.value);
+      });
 
-				// views annotations
-				deepApply(stateChunk, [{
-					"type": "Property",
-					"key": {
-						"type": "Identifier",
-						"name": "views"
-					},
-					"value": {
-						"type": "ObjectExpression"
-					}
-				}], function (viewsChunk) {
-					deepApply(viewsChunk, [{
-						"type": "Property",
-						"key": {
-							"type": "Identifier"
-						},
-						"value": {
-							"type": "ObjectExpression"
-						}
-					}], function (viewsIdentifierChunk) {
-						deepApply(viewsIdentifierChunk, [{
-							"type": "Property",
-							"key": {
-								"type": "Identifier",
-								"name": "controller"
-							},
-							"value": {
-								"type": "FunctionExpression"
-							}
-						}], function (controllerIdentifierChunk) {
-							controllerIdentifierChunk.value = annotateInjectable(controllerIdentifierChunk.value);
-						});
-					});
-				});
-			});
-		});
-	}
+      // resolve annoations
+      deepApply(stateChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "resolve"
+        },
+        "value": {
+          "type": "ObjectExpression"
+        }
+      }], function (resolveChunk) {
+        deepApply(resolveChunk, [{
+          "type": "Property",
+          "key": {
+            "type": "Identifier"
+          },
+          "value": {
+            "type": "FunctionExpression"
+          }
+        }], function (resolveIdentifierChunk) {
+          resolveIdentifierChunk.value = annotateInjectable(resolveIdentifierChunk.value);
+        });
+      });
+
+      // views annotations
+      deepApply(stateChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "views"
+        },
+        "value": {
+          "type": "ObjectExpression"
+        }
+      }], function (viewsChunk) {
+        deepApply(viewsChunk, [{
+          "type": "Property",
+          "key": {
+            "type": "Identifier"
+          },
+          "value": {
+            "type": "ObjectExpression"
+          }
+        }], function (viewsIdentifierChunk) {
+          deepApply(viewsIdentifierChunk, [{
+            "type": "Property",
+            "key": {
+              "type": "Identifier",
+              "name": "controller"
+            },
+            "value": {
+              "type": "FunctionExpression"
+            }
+          }], function (controllerIdentifierChunk) {
+            controllerIdentifierChunk.value = annotateInjectable(controllerIdentifierChunk.value);
+          });
+        });
+      });
+    });
+  });
 };
